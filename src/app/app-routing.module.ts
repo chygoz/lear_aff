542fc8295ada114110fbcdec0513d401e08398e6
Ram <ramanjaneyulu901@gmail.com>
2019-11-22 22:51:17 +0530

    base setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReportsComponent } from './reports/reports.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AffiliatesComponent } from './affiliates/affiliates.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SidebarComponent,
+ },
+  {
+    path: 'home',
+    component: HomeComponent,
+ },
+ {
+  path: 'reports',
+  component: ReportsComponent,
+},
+{
+  path: 'statistics',
+  component: StatisticsComponent,
+},
+{
+  path: 'affiliates',
+  component: AffiliatesComponent,
+},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReportsComponent } from './reports/reports.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AffiliatesComponent } from './affiliates/affiliates.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ReportsComponent,
+    StatisticsComponent,
+    AffiliatesComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
