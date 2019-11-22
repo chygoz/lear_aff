e13ac1e1f35469b509a1dbd087658b5f89e837b7
Ram <ramanjaneyulu901@gmail.com>
2019-11-23 02:14:02 +0530

    Reports UI

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   //styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent implements OnInit {
+  reports:any = []
 
   constructor() { }
 
   ngOnInit() {
+    this.reports = [
+      {
+        name: 'Jonah Kenos',
+        currentWeek: '345',
+        lastWeek: '456',
+        totalPoints: '965'
+      },
+      {
+        name: 'Dangone Bella',
+        currentWeek: '345',
+        lastWeek: '456',
+        totalPoints: '965'
+      },
+      {
+        name: 'Micheal Boltton',
+        currentWeek: '345',
+        lastWeek: '456',
+        totalPoints: '965'
+      },
+      {
+        name: 'Jonah Kenos',
+        currentWeek: '345',
+        lastWeek: '456',
+        totalPoints: '965'
+      },
+      {
+        name: 'Dangone Bella',
+        currentWeek: '345',
+        lastWeek: '456',
+        totalPoints: '965'
+      },
+      {
+        name: 'Micheal Boltton',
+        currentWeek: '345',
+        lastWeek: '456',
+        totalPoints: '965'
+      }
+
+
+
+    ]
   }
 
 }
