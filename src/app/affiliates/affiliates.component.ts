95a850b1b074a048a653ff498ff02e112209c568
Ram <ramanjaneyulu901@gmail.com>
2019-11-23 02:27:43 +0530

    Affiliate UI

--- src/app/affiliates/affiliates.component.ts
+++ src/app/affiliates/affiliates.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   //styleUrls: ['./affiliates.component.css']
 })
 export class AffiliatesComponent implements OnInit {
-
+  affiliateUsers:any = []
   constructor() { }
 
   ngOnInit() {
+    this.affiliateUsers = [
+      {
+        name: 'Jonah Kenos',
+        referalCount: '345',
+        points: '456',
+        amount: '965'
+      },
+      {
+        name: 'Dangone Bella',
+        referalCount: '345',
+        points: '456',
+        amount: '965'
+      },
+      {
+        name: 'Micheal Boltton',
+        referalCount: '345',
+        points: '456',
+        amount: '965'
+      },
+      {
+        name: 'Jonah Kenos',
+        referalCount: '345',
+        points: '456',
+        amount: '965'
+      },
+      {
+        name: 'Dangone Bella',
+        referalCount: '345',
+        points: '456',
+        amount: '965'
+      },
+      {
+        name: 'Micheal Boltton',
+        referalCount: '345',
+        points: '456',
+        amount: '965'
+      }
+    ]
   }
 
 }
