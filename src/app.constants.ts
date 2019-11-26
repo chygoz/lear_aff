a1cd3f52090be441904393f4d1a94d0af86f2849
Sai Kiran <sainik18@gmail.com>
2019-11-27 03:40:37 +0530

    Update app.constants.ts

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,9 +1,9 @@
 export const api = {
     login: 'https://lear-one.herokuapp.com/api/admin/login',
     //api_url: 'http://159.65.82.43:3000/api/',
-    //api_url: 'https://sportsetrader.com/api/',
+    api_url: 'https://sportsetrader.com/api/',
     image_url: 'https://sportsetrader.com/',
-     api_url: 'http://localhost:3000/api/',
+     //api_url: 'http://localhost:3000/api/',
     //api: 'https://localhost:3000/'
      //api: 'https://sportsetrader.com/',
 }
